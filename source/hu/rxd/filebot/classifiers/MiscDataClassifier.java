9bc7d23ea5a5b989b2904a38cddef5e0f8bd9c70
Haindrich Zoltán (kirk) <kirk@rxd.hu>
2015-11-26 21:48:31 +0100

    update misc removal

--- source/hu/rxd/filebot/classifiers/MiscDataClassifier.java
+++ source/hu/rxd/filebot/classifiers/MiscDataClassifier.java
@@ -1,5 +1,12 @@
 package hu.rxd.filebot.classifiers;
 
+import static java.util.regex.Pattern.CASE_INSENSITIVE;
+import static java.util.regex.Pattern.UNICODE_CHARACTER_CLASS;
+import static java.util.regex.Pattern.compile;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import hu.rxd.filebot.normalization.SuffixRemoval;
 import hu.rxd.filebot.tree.MediaSection;
 import hu.rxd.filebot.tree.TypeTags;
@@ -11,21 +18,27 @@ public class MiscDataClassifier implements ISectionVisitor {
 	@Override
 	public void visit(ISection node) throws Exception {
 		
-		String startPatterns[]={"DVDRip","BDRIP","HDTV","hdtv","720p","720P","x264","Hdtv"};
+		
+		Pattern pat = compile("(REPACK|DVDRip|BDRIP|HDTV|hdtv|720p|720p|x264|Hdtv).+", CASE_INSENSITIVE | UNICODE_CHARACTER_CLASS);
+
+//		String startPatterns[]={"DVDRip","BDRIP","HDTV","hdtv","720p","720p","x264","Hdtv"};
 		String n = node.getName();
 		int	start=-1;
-		for (String pat : startPatterns) {
-			int i = (n.indexOf(pat));
-			if(i>=0){
-				if(start == -1 || start > i){
-					start=i;
-				}
-			}
+		Matcher matcher = pat.matcher(n);
+		if(matcher.find()){
+//			String suffix=n.substring(start);
+			node.addNormalization(new SuffixRemoval(TypeTags.MISC, matcher.group(0)));
 		}
-		if(start>=0){
-			String suffix=n.substring(start);
-			node.addNormalization(new SuffixRemoval(TypeTags.MISC, suffix));
-		}
+//		for (String pat : startPatterns) {
+//			int i = (n.indexOf(pat));
+//			if(i>=0){
+//				if(start == -1 || start > i){
+//					start=i;
+//				}
+//			}
+//		}
+//		if(start>=0){
+//		}
 		System.out.println(node);
 
 	}
--- test/hu/rxd/ClassificationIntegrationTests.java
+++ test/hu/rxd/ClassificationIntegrationTests.java
@@ -54,7 +54,27 @@ public class ClassificationIntegrationTests {
 //		assertEquals("20",leaf.getTagByName("series").getValue());
 
 	}
+	@Test
+	public void empire() throws Exception {
+		
+		Root root = basicSections("Empire S01 1080p", "1x10 - Sins Of The Father 1080p Bluray.mkv");
+		DirectoryScanner.tagDecorator1(root);
+		ISection leaf = getLeaf(root);
+		assertEquals("10",leaf.getTag(MediaTagKey.episode).getValue());
+		assertEquals("1",leaf.getTag(MediaTagKey.season).getValue());
+		assertEquals("Empire",leaf.getTag(MediaTagKey.series).getValue());
+	}
 
+	
+	@Test
+	public void psych() throws Exception {
+		Root root = basicSections("Psych.S03.HUN.DVDRip.XviD-HSF","Psych.S03E15.REPACK.HUN.DVDRip.XviD-HSF","hsf-psych-rpck-315.avi");
+		DirectoryScanner.tagDecorator1(root);
+		ISection leaf = getLeaf(root);
+		assertEquals("15",leaf.getTag(MediaTagKey.episode).getValue());
+		assertEquals("3",leaf.getTag(MediaTagKey.season).getValue());
+		assertEquals("Psych",leaf.getTag(MediaTagKey.series).getValue());
+	}
 	private ISection getLeaf(Root root) {
 		ISection curr = root;
 		while (curr.getChildren().size() > 0) {
