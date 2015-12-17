73eee582288f8b888016a118778282cf1e1f5f30
Haindrich Zoltán (kirk) <kirk@rxd.hu>
2015-12-17 23:15:39 +0100

    reload previously extracted files

--- source/hu/rxd/filebot/classifiers/ExtractionRunner.java
+++ source/hu/rxd/filebot/classifiers/ExtractionRunner.java
@@ -1,7 +1,11 @@
 package hu.rxd.filebot.classifiers;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 import com.google.common.collect.Sets;
 
@@ -21,20 +25,40 @@ public class ExtractionRunner implements ISectionVisitor {
 	public void visit(ISection node) throws Exception {
 		
 		CmdlineOperations cmdline = new CmdlineOperations();
+		File extractDir = new File(extractionRoot,node.getName());
 		List<File> ef = cmdline.extract(Sets.newHashSet(node.getAbsoluteFile()),
-				new File(extractionRoot,node.getName()).getAbsolutePath(),
+				extractDir.getAbsolutePath(),
 				ConflictAction.AUTO.toString(),
 				null,
 				false);
 		
-		for (File file : ef) {
+//		flatFileList(extractDir);
+		
+		for (File file : flatFileList(extractDir)) {
 			node.getParent().getShadowEntry(file);
 		}
 		System.out.println(ef);
+		System.out.println(flatFileList(extractDir));
 //		MediaCollection c = (MediaCollection)node.getParent();
 		
 //		c.getEntry(name);
 		
+	}
+
+	private Collection<File> flatFileList(File dir) {
+		List<File>	ret=new ArrayList<>();
+		
+		Queue<File>	q=new PriorityQueue<>();
+		q.add(dir);
+		while(!q.isEmpty()){
+			File f = q.poll();
+			if(f.isDirectory()){
+				q.add(f);
+			}else{
+				ret.add(f);
+			}
+		}
+		return ret;
 	}
 
 }
