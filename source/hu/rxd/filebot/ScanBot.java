c024a0bb1ef1eacda6fb825aa1d0e417b7f801a4
Haindrich Zoltán (kirk) <kirk@rxd.hu>
2016-04-19 22:43:54 +0200

    fix typo of statefile

--- source/hu/rxd/filebot/ScanBot.java
+++ source/hu/rxd/filebot/ScanBot.java
@@ -87,7 +87,7 @@ public class ScanBot {
 	private void run() throws Exception {
 		
 		src = new File(srcDir);
-		stateFile = new File(src,STATEFILE_NAME);
+		stateFile = new File(stateDir,STATEFILE_NAME);
 		
 		if(!src.exists() || !src.isDirectory()){
 			throw new IllegalArgumentException(String.format("source directory: %s doesnt exists, or not a directory", srcDir));
